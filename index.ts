5d20ec2c54a7663176c38efd934c7482589dd9b3
Zill-e-kazim <kazim1231@gmail.com>
2023-05-10 17:45:46 +0500

    Add apis

---
+++ index.ts
@@ -0,0 +1,60 @@
+import cors, { CorsOptions } from "cors";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import fs from "fs/promises";
+import multer, { diskStorage } from "multer";
+
+const storage = diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.body.fileName);
+  },
+});
+
+const upload = multer({ storage });
+
+interface IData {
+  fileName: string;
+  transript: string;
+  isCompleted: boolean;
+}
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+const corsOptions: CorsOptions = {
+  origin: ["http://localhost:3000"],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true,
+  preflightContinue: true,
+};
+app.use(cors(corsOptions));
+
+app.options("*", cors());
+
+app.get("/", async (req: Request, res: Response) => {
+  const json: IData[] = JSON.parse(await fs.readFile("data.json", "utf-8"));
+  res.json(json.filter((i) => !i.isCompleted)[0]);
+});
+
+app.post(
+  "/submit",
+  upload.single("recording"),
+  async (req: Request, res: Response) => {
+    const fileName = req.query["fileName"] as string;
+    const json: IData[] = JSON.parse(await fs.readFile("data.json", "utf-8"));
+    const index = json.findIndex((i) => i.fileName === fileName);
+    json[index].isCompleted = true;
+    await fs.writeFile("data.json", JSON.stringify(json));
+
+    res.status(200).send();
+  }
+);
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
